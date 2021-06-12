ac8c5e24146bcafab81bca696aa56e898d69036c
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-06-12 14:41:22 -0500

    Update Keyboard_Matrix.h

--- Keyboard_Matrix.h
+++ Keyboard_Matrix.h
@@ -3,6 +3,13 @@
 
 #include "fsl_gpio.h"
 
+#define PIN0       	0u
+#define PIN1       	1u
+#define PIN2		2u
+#define PIN3		3u
+#define PIN10		10u
+#define PIN11		11u
+
 void Read_KeyPad(L1, L2, L3, L4, C1, C2, C3, C4, B1, B2);
 
 #endif /* KEYBOARD_MATRIX_H_ */
