f7b8f836a746cd87db2b7a471ffe97ff0c4949a6
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-06-12 22:46:02 -0500

    Create PIT.c

---
+++ PIT.c
@@ -0,0 +1,15 @@
+#include "PIT.h"
+
+uint8_t PIT_Flag_get_PIT(void){
+	return (pitIsrFlag);
+}
+
+void PIT0_IRQHandler(void){
+	PIT_ClearStatusFlags(PIT, kPIT_Chnl_0, kPIT_TimerFlag);
+	pitIsrFlag = true;
+	__DSB();
+}
+
+void PIT_Flag_Set_PIT(void){
+	pitIsrFlag = false;
+}
