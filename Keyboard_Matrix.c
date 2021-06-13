8b1b97cd46d11867e70a78b3f79e8cf18d434ba0
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-06-12 19:38:21 -0500

    Update Keyboard_Matrix.c

--- Keyboard_Matrix.c
+++ Keyboard_Matrix.c
@@ -1,15 +1,65 @@
 #include "Keyboard_Matrix.h"
 
 void Read_KeyPad(L1, L2, L3, L4, C1, C2, C3, C4){
-	printf("L1: %d \n", L1);
-	printf("L2: %d \n", L2);
-	printf("L3: %d \n", L3);
-	printf("L4: %d \n", L4);
-	printf("C1: %d \n", C1);
-	printf("C2: %d \n", C2);
-	printf("C3: %d \n", C3);
-	printf("C4: %d \n", C4);
 
+	GPIO_PortSet(GPIOB, 1u << PIN2);
+	if(C1 == 1){
+		printf("1\n");
+	}
+	else if(C2 == 1){
+		printf("2\n");
+	}
+	else if(C3 == 1){
+		printf("3\n");
+	}
+	else if(C4 == 1){
+		printf("A\n");
+	}
+	GPIO_PortClear(GPIOB, 1u << PIN2);
 
+	GPIO_PortSet(GPIOB, 1u << PIN3);
+	if(C1 == 1){
+		printf("4\n");
+	}
+	else if(C2 == 1){
+		printf("5\n");
+	}
+	else if(C3 == 1){
+		printf("6\n");
+	}
+	else if(C4 == 1){
+		printf("B\n");
+	}
+	GPIO_PortClear(GPIOB, 1u << PIN3);
+
+	GPIO_PortSet(GPIOB, 1u << PIN10);
+	if(C1 == 1){
+		printf("7\n");
+	}
+	else if(C2 == 1){
+		printf("8\n");
+	}
+	else if(C3 == 1){
+		printf("9\n");
+	}
+	else if(C4 == 1){
+		printf("C\n");
+	}
+	GPIO_PortClear(GPIOB, 1u << PIN10);
+
+	GPIO_PortSet(GPIOB, 1u << PIN11);
+	if(C1 == 1){
+		printf("*\n");
+	}
+	else if(C2 == 1){
+		printf("0\n");
+	}
+	else if(C3 == 1){
+		printf("#\n");
+	}
+	else if(C4 == 1){
+		printf("D\n");
+	}
+	GPIO_PortClear(GPIOB, 1u << PIN11);
 
 }
