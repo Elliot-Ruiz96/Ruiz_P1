5d3a4980c30fcf77252f55b0ab0a53b9c9813689
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-06-14 09:28:50 -0500

    Update PIT.c

--- PIT.c
+++ PIT.c
@@ -15,3 +15,16 @@ void PIT0_IRQHandler(void){
 void PIT_Flag_Set_PIT(void){
 	pitIsrFlag = false;
 }
+
+void PIT_Initialize(){
+
+	pit_config_t pitConfig;
+	uint32_t FREQ = 0;
+	PIT_GetDefaultConfig(&pitConfig);
+	PIT_Init(PIT, &pitConfig);
+	FREQ = CLOCK_GetFreq(kCLOCK_BusClk);
+	PIT_SetTimerPeriod(PIT, kPIT_Chnl_0, USEC_TO_COUNT(1000000U, FREQ));
+	PIT_EnableInterrupts(PIT, kPIT_Chnl_0, kPIT_TimerInterruptEnable);
+	EnableIRQ(PIT0_IRQn);
+	PIT_StartTimer(PIT, kPIT_Chnl_0);
+}
