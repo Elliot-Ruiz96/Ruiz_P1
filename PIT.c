cc86ab4d462a4248ad4ffc08d9d078d573e19b9e
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-06-17 08:49:49 -0500

    Update PIT.c

--- PIT.c
+++ PIT.c
@@ -2,6 +2,7 @@
 
 volatile bool pitIsrFlag = false;
 volatile bool pitIsrFlag1 = false;
+uint32_t PIT_Value = 1000000;
 
 uint8_t PIT_Flag_get_PIT(void){
 	return (pitIsrFlag);
@@ -50,8 +51,12 @@ void PIT1_Initialize(){
 	PIT_GetDefaultConfig(&pitConfig);
 	PIT_Init(PIT, &pitConfig);
 	FREQ1 = CLOCK_GetFreq(kCLOCK_BusClk);
-	PIT_SetTimerPeriod(PIT, kPIT_Chnl_1, USEC_TO_COUNT(1000000U, FREQ1));
+	PIT_SetTimerPeriod(PIT, kPIT_Chnl_1, USEC_TO_COUNT(PIT_Value, FREQ1));
 	PIT_EnableInterrupts(PIT, kPIT_Chnl_1, kPIT_TimerInterruptEnable);
 	EnableIRQ(PIT1_IRQn);
 	PIT_StartTimer(PIT, kPIT_Chnl_1);
+}
+
+void PIT_Start(total){
+	PIT_Value = total;
 }
