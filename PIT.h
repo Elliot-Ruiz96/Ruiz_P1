337de79840ab145ea315ee28a91cb6b9f2588bae
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-06-17 08:44:40 -0500

    Update PIT.h

--- PIT.h
+++ PIT.h
@@ -5,11 +5,13 @@
 #include "fsl_pit.h"
 
 uint8_t PIT_Flag_get_PIT(void);
-
 void PIT0_IRQHandler(void);
-
 void PIT_Flag_Set_PIT(void);
-
 void PIT_Initialize();
+
+uint8_t PIT_Flag_get_PIT1(void);
+void PIT1_IRQHandler(void);
+void PIT_Flag_Set_PIT1(void);
+void PIT1_Initialize();
 
 #endif /* PIT_H_ */
